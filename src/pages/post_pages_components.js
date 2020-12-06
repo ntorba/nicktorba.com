fa7f01a608508e86d06daefaf377272a65c122c9
ntorba <32570754+ntorba@users.noreply.github.com>
2020-12-06 09:22:50 -0500

    Create post_pages_components.js

---
+++ src/pages/post_pages_components.js
@@ -0,0 +1,40 @@
+import styled from "@emotion/styled";
+import { colors } from "../tokens";
+import { Link } from "gatsby";
+
+export const AllPosts = styled.div``;
+
+export const Post = styled(Link)`
+  display: block;
+  margin-bottom: 45px;
+
+  p {
+    color: ${colors.gray800};
+  }
+
+  :hover p {
+    color: ${colors.gray500};
+  }
+`;
+
+export const PostTitle = styled.h3`
+  color: ${colors.purple900};
+
+  ${Post}:hover & {
+    color: ${colors.purple500};
+  }
+`;
+
+export const PostDate = styled.div`
+  width: 100%;
+  max-width: 600px;
+  margin-top: -10px;
+  margin-bottom: 15px;
+  font-size: 11pt;
+  color: ${colors.gray500};
+
+  ${Post}:hover & {
+    color: ${colors.gray400};
+  }
+`;
+
